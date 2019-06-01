5105ee20c28ff6e1c8921df49d2b070d92f43551
wcx <38618102@qq.com>
2019-06-01 11:15:53 +0800

    在git工作空间修改了文件

--- src/com/newroad/test/test.java
+++ src/com/newroad/test/test.java
@@ -2,9 +2,17 @@ package com.newroad.test;
 
 public class test {
 	
-	public void testGit() {
-		System.out.println("第一次操作git管理java项目");
-		System.out.println("修改本地仓库");
+	public static void main(String[] args) {
+		
+		
+			System.out.println("第一次操作git管理java项目");
+			System.out.println("修改本地仓库");
+			
+			System.out.println("在git工作空间修改了文件");
+			
+	
+		
 	}
+	
 
 }
